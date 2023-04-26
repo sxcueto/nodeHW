e051ebf920413747401a50f97051fe0b0bd2dd1b
unknown <sxcueto@gmail.com>
2023-04-25 20:29:44 -0400

    push

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
+const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  
+];
+
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html', 
-    {root:__dirname})
-})
+    res.render('index', { title:'Home', blogs});
+});
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html', 
-    {root:__dirname})
-})
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about')
-})
+    res.render('about', { title:'About'});
+});
+app.get('/blogs/create',(req,res)=>{
+    res.render('create', { title:'Create a new blog'});
+});
+
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).render('404', { title:'404'});
     
-})
+});
