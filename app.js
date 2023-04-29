85cbe2fea89e0e943185e7b1b4b8940bbd441578
unknown <sxcueto@gmail.com>
2023-04-28 23:22:09 -0400

    push

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use(morgan('dev'));
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
 const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -13,14 +18,15 @@ const blogs = [
   
 ];
 
-app.get('/',(req,res)=>{
-    res.render('index', { title:'Home', blogs});
+ res.render('index', { title: 'Home', blogs});
+   
 });
+
 app.get('/about',(req,res)=>{
-    res.render('about', { title:'About'});
+    res.render('about', { title: 'About'});
 });
-app.get('/blogs/create',(req,res)=>{
-    res.render('create', { title:'Create a new blog'});
+app.get('/create',(req,res)=>{
+    res.render('create', { title: 'Create a new blog'});
 });
 
 
@@ -28,3 +34,4 @@ app.use((req,res)=>{
     res.status(404).render('404', { title:'404'});
     
 });
+
